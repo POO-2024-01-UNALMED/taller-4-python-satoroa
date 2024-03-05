cf5a3577c7a7c7d970ccf5e786e597302e70dd33
satoroa <satoroa@unal.edu.co>
2024-03-04 20:34:33 -0500

    correccion 14

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,11 +1,9 @@
 class Asignatura:
-
-    def __init__(self, nombre, salon="remoto"):
+    def __init__(self, nombre, salon=None):
         self._nombre = nombre
         self._salon = salon
 
-    def __str__(self):
-        return f"{self._nombre} {self._salon}"
-
-    # def __str__(self):
-    #     pass
+    def __eq__(self, other):
+        if not isinstance(other, Asignatura):
+            return False
+        return self._nombre == other._nombre and self._salon == other._salon
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,38 +1,16 @@
-from classroom.asignatura import Asignatura
-
 class Grupo:
+    def __init__(self, nombre, asignaturas, alumnos):
+        self._nombre = nombre
+        self._asignaturas = asignaturas
+        self._alumnos = alumnos
 
-    grado = "Grado 12"
+    def agregarAlumno(self, nombre, asignaturas):
+        self._alumnos.append(Alumno(nombre, asignaturas))
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, alumnos=None):
-        self._grupo = grupo
-        self._asignaturas = asignaturas if asignaturas is not None else []
-        self.listadoAlumnos = alumnos if alumnos is not None else []
-        self.__class__.grado = "Grado 12"
+    def listadoAlumnos(self):
+        return [alumno.nombre for alumno in self._alumnos]
 
-    def agregarAlumno(self, *alumnos):
-        for alumno in alumnos:
-            if isinstance(alumno, list):
-                self.listadoAlumnos.extend(alumno)
-            else:
-                self.listadoAlumnos.append(alumno)
-        self._grupo = f"Grupo {len(self.listadoAlumnos)}"
-
-    def listadoAsignaturas(self, **asignaturas):
-        for nombre, salon in asignaturas.items():
-            if salon is None:
-                asignatura = Asignatura(nombre)
-            else:
-                asignatura = Asignatura(nombre, salon)
-            self._asignaturas.append(asignatura)
-
-    @classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    def __str__(self):
-        return f"Grupo de estudiantes: {self._grupo}"
-
-    @property
-    def grado(self):
-        return self.__class__.grado
+    def __eq__(self, other):
+        if not isinstance(other, Grupo):
+            return False
+        return self._nombre == other._nombre and self._alumnos == other._alumnos
